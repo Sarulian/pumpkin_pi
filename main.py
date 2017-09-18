21b0422d822cc28be75bd0f737770d9602b31be2
Sarulian <tedjamesrobinson@gmail.com>
2017-09-18 16:40:23 -0700

    testing popen

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import subprocess as sub
+import time
+
+
+def light_LED(pin_number, time, brightness):
+	print("Do stuff")
 
 
 def main():
 	#initialize GPIO pins
-	# GPIO.setmode(GPIO.BOARD)
+	blue_led = 33
+	red_led = 32
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(blue_led, GPIO.OUT)
+	GPIO.setup(red_led, GPIO.OUT)
 
-	# path = sub.Popen(['ls','-a'],stdout=sub.PIPE,stderr=sub.PIPE)
-	# output, errors = path.communicate()
-	# print(output)
+	time.sleep(1)
 
-	sub.run(['mpg321', 'monstermoan.mp3'])
+	while(True):
+		print("Blue on.")
+		GPIO.output(blue_led, 1)
+		GPIO.output(red_led, 0)
+
+		time.sleep(2)
+
+		print("Red on.")
+		GPIO.output(blue_led, 0)
+		GPIO.output(red_led, 1)
+
+		time.sleep(1)
+
+	# sub.run(['mpg321','monstermoan.mp3'])
 
 
 if __name__ == "__main__":
