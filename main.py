a7535c65f825b2af17f9748360c0c39f382cc6e4
Sarulian <tedjamesrobinson@gmail.com>
2017-09-18 16:08:10 -0700

    testing popen

---
+++ main.py
@@ -0,0 +1,15 @@
+# import RPi.GPIO as GPIO
+import suprocess as sub
+
+
+def main():
+	#initialize GPIO pins
+	# GPIO.setmode(GPIO.BOARD)
+	
+	path = sub.Popen(['ls','-a'],stdout=sub.PIPE,stderr=sub.PIPE)
+	output, errors = p.communicate()
+	print(output)
+
+
+if __name__ == "__main__":
+	main()
