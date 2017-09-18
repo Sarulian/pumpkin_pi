b02bc18a6d033dd0924ad88a7c57e691d7d89b3f
Sarulian <tedjamesrobinson@gmail.com>
2017-09-18 16:12:02 -0700

    testing popen

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ def main():
 	#initialize GPIO pins
 	# GPIO.setmode(GPIO.BOARD)
 
-	path = sub.Popen(['ls','-a'],stdout=sub.PIPE,stderr=sub.PIPE)
-	output, errors = path.communicate()
-	print(output)
+	# path = sub.Popen(['ls','-a'],stdout=sub.PIPE,stderr=sub.PIPE)
+	# output, errors = path.communicate()
+	# print(output)
+
+	sub.run('mpg321', 'monstermoan.mp3')
 
 
 if __name__ == "__main__":
