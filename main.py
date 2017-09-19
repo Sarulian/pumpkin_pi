1fbd9b0c6a68c0d745d659dea67c2da46836de85
Sarulian <tedjamesrobinson@gmail.com>
2017-09-18 21:58:02 -0700

    fully functional with shock, heartbeat, moans and commented for clarity

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ def main():
 	# main loop
 	while True:
 		if GPIO.input(start_button):
+
+			# emmit shock sounds of random lengths with blue LED
 			for i in range(r.randint(3,12)):
 				total = 55
 				length = r.randint(int(total/7),int(total/2))
@@ -39,18 +41,18 @@ def main():
 				time.sleep(r.random()/3)
 				i += 1
 
+			# one last shock of max length
 			turn_on(blue_led)
-			sub.run(['mpg321','ESPARK1.mp3', '-k 0', '-n 55'])
+			sub.run(['mpg321','ESPARK1.mp3'])
 			turn_off(blue_led)
 
 
+			# play heartbeat sound with red led pulsing in beat
 			play_sound = sub.Popen(['mpg321','heartbeat.mp3','-l 3'])
-
 			freq = 200
 			duty = 0
 			brightening = True
 			p = GPIO.PWM(red_led, freq)
-			
 			p.start(duty)
 			while True:
 				p.ChangeDutyCycle(duty)
@@ -70,6 +72,7 @@ def main():
 				except sub.TimeoutExpired:
 					continue
 
+			# monster moan to finish it off
 			turn_on(blue_led)
 			p.ChangeDutyCycle(100)
 			sub.run(['mpg321','monstermoan.mp3'])
