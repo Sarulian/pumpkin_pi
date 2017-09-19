10791c9c525419d48d8b908ca677df3adbad4091
Sarulian <tedjamesrobinson@gmail.com>
2017-09-18 17:31:35 -0700

    testing popen

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
 		turn_on(blue_led)
 		sub.run(['mpg321','Electric.mp3', '-k {}'.format(start), '-n {}'.format(start+length)])
 		turn_off(blue_led)
-		time.sleep(int(length/4))
+		time.sleep(r.randrange(0,2,0.1))
 
 
 if __name__ == "__main__":
